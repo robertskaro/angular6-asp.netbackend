cdd1e62a9b8bf1c31a606ca53f3c1842d908b5b0
ROBI-PREDATOR\robi <robert@index.hr>
2018-09-07 18:17:23 +0200

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly string rootViewPath = "~/wwwroot/Index.cshtml";
+        public IActionResult Index()
+        {
+            AppendHeaderValues("home");
+            return View(rootViewPath);
+        }
+
+        [Route("some/dude")]
+        public IActionResult somedude()
+        {
+            AppendHeaderValues("someDude");
+            return View(rootViewPath);
+        }
+
+
+        private void AppendHeaderValues(string value)
+        {
+            //*if user agent == facebook
+            ViewBag.SomeCrazyHeader = value;
+        }
+    }
+}
